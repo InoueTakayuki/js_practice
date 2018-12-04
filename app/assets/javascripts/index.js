bc03e741fb2671f3be2d01088ac579d306a9cd65
InoueTakayuki <takayuki.inoue527@gmail.com>
2018-12-04 13:02:46 +0900

    js.practice

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,14 @@
+var items = [
+  { name: 'じゃがいも', price: 50, count: 3 },
+  { name: 'にんじん', price: 60, count: 1 },
+  { name: '豚肉', price: 200, count: 1 },
+  { name: '玉ねぎ', price: 50, count: 2 }
+];
+console.log(items)
+
+// targetPriceより小計の高額な商品を出力
+function higherPrice (targetPrice) {
+  // ここにソースコードを追記しましょう
+}
+
+higherPrice(120);
